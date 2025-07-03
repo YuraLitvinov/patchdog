e828ee804e9328ce40d945aecbc4d0f135926673
YuraLitvinov <litvinov.yura@gmail.com>
2025-07-03 15:48:26 +0200

    added tests.rs

--- crates/patchdog/src/main.rs
+++ crates/patchdog/src/main.rs
@@ -2,6 +2,7 @@ use dotenv::dotenv;
 use patchdog::receive_context;
 use std::env;
 use std::path::Path;
+pub mod tests;
 #[tokio::main]
 
 async fn main() {
@@ -21,31 +22,3 @@ async fn main() {
     println!("{}", formatted_receive);
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn testing_seeker_for_basic() {
-        let string_of_func = "use filesystem_parsing::parse_all_rust_items;";
-        let path = Path::new("/home/runner/work/patchdog/patchdog/crates/patchdog/src/lib.rs");
-        let receive = receive_context(1, path);
-        let formatted_receive = receive.unwrap();
-        assert_eq!(formatted_receive, string_of_func);
-    }
-    #[test]
-    fn testing_seeker_for_zero() {
-        let string_of_func: &'static str = "Line index 0 is out of bounds";
-        let path = Path::new("/home/runner/work/patchdog/patchdog/crates/patchdog/src/lib.rs");
-        let receive = receive_context(0, path);
-        let formatted_receive = receive.unwrap();
-        assert_eq!(formatted_receive, string_of_func);
-    }
-    #[test]
-    fn testing_seeker_for_out_of_bounds() {
-        let string_of_func: &'static str = "Line index 10000 is out of bounds";
-        let path = Path::new("/home/runner/work/patchdog/patchdog/crates/patchdog/src/lib.rs");
-        let receive = receive_context(10000, path);
-        let formatted_receive = receive.unwrap();
-        assert_eq!(formatted_receive, string_of_func);
-    }
-}
