bc05fa532f9a42f80180d58b8ba8ed75b3c3e324
YuraLitvinov <litvinov.yura@gmail.com>
2025-07-17 21:10:44 +0200

    Fns parse (#15)
    * cli interface, wrapper for patch case and filepath case
    
    * added main

--- crates/patchdog/src/main.rs
+++ crates/patchdog/src/main.rs
@@ -1,12 +1,11 @@
-//https://interoperable-europe.ec.europa.eu/sites/default/files/custom-page/attachment/2020-03/EUPL-1.2%20EN.txt
 use crate::binding::ErrorBinding;
-use crate::cli::cli_patch_mode;
+use crate::cli::{cli_search_mode};
 pub mod binding;
 pub mod cli;
 #[cfg(test)]
 pub mod tests;
 #[tokio::main]
 async fn main() -> Result<(), ErrorBinding> {
-    cli_patch_mode()?;
+    cli_search_mode().await?;
     Ok(())
 }
