2cae77d3cb27b0595b26ba64387a96fbcdb87354
YuraLitvinov <litvinov.yura@gmail.com>
2025-08-15 15:07:29 +0200

    boilerplace branch to trigger workflow (#30)
    * boilerplace branch to trigger workflow
    
    * Patchdog-68506e33-0bf5-4e5e-bfc0-fef85f2b389a (#31)
    
    Co-authored-by: Patchdog Patchdogovich <noreply@patchdog.ua>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Patchdog Patchdogovich <noreply@patchdog.ua>

--- crates/patchdog/src/binding.rs
+++ crates/patchdog/src/binding.rs
@@ -123,9 +123,10 @@ pub fn changes_from_patch(
 
 //Seeking context inside same file, to match probable structures
 //Checking uses, to limit amount of crates to be parsed
-//Allocating  
-//Here importing LocalChange to know path to function, function_text as information to grab
-//Then, we identify all uses inside the function, to limit the scope of search even further
+//Instead of parsing whole project - we parse few of the crates
+///   Identifies and extracts the source code of external Rust dependencies for a specified function within a project.
+///   It first parses `use` statements from the file at `change`, then collects all related Rust files, and subsequently extracts code segments from these files that are referenced within the provided `function_text`.
+///   The function returns a `Context` object, containing a list of strings where each string represents the source code of a detected external dependency, excluding the function itself.
 pub fn find_context (change: PathBuf, fn_name: &str, function_text: &String) -> Result<Context, ErrorBinding> {
     let mut context = vec![];
     //Crate level-context seeking
@@ -135,9 +136,8 @@ pub fn find_context (change: PathBuf, fn_name: &str, function_text: &String) -> 
     //We can reach them and get the context from there
     //Hashmaps doesn't allow matching certain crate more than once
     //Now we want to find all use statements, to make scope of search smaller. 
-    //Instead of parsing whole project - we parse few of the crates
     let all_uses = parse_use(parsed.items);
-    //Here is map_rust_files we locate all the crates used within the file where function is located
+    //Here in map_rust_files we locate all the crates used within the file where function is located
     let map_rust_files = collect_paths(all_uses.clone(), change.clone())?;
     let use_map = all_uses.par_iter().map(|single_use| 
         (single_use.object.to_owned(), single_use.to_owned()) 
