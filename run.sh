787af08466c4239122197076cef14e67c36839d5
Yurii Litvinov <litvinov.yura@gmail.com>
2025-08-16 23:21:47 +0200

    updated

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+echo "NAME: $COMMIT_NAME $COMMIT_EMAIL "
+
 git fetch origin ${BASE_BRANCH}
 
 #Test if PR contains conflict and abort any further actions
@@ -11,7 +13,7 @@ fi
 #Download and run latest release
 curl -L -o patchdog-linux-x86_64 https://github.com/YuraLitvinov/patchdog/releases/latest/download/patchdog
 chmod +x patchdog-linux-x86_64
-git diff origin/${BASE_BRANCH}...${HEAD_BRANCH} > base_head.patch
+git diff origin/$BASE_BRANCH...$HEAD_BRANCH > base_head.patch
 ./patchdog-linux-x86_64 --file-patch base_head.patch
 #Cleanup artifacts
 rm base_head.patch && rm patchdog-linux-x86_64
@@ -19,19 +21,18 @@ rm base_head.patch && rm patchdog-linux-x86_64
 #Configure user
 git config --global user.email "$COMMIT_EMAIL" 
 git config --global user.name "$COMMIT_NAME"
-echo "NAME: $COMMIT_NAME $COMMIT_EMAIL "
 
 #Create a unique pull request
 PATCHDOG_BRANCH="patchdog-$(uuidgen)"
 
 git checkout -b "$PATCHDOG_BRANCH"
 git add . ':(exclude).github/workflows/'
-git commit -m "Patchdog-included changes for ${HEAD_BRANCH}"
+git commit -m "Patchdog-included changes for $HEAD_BRANCH"
 git push origin $PATCHDOG_BRANCH
 gh pr create \
-  --title "Patchdog merge into ${HEAD_BRANCH}" \
+  --title "Patchdog merge into $HEAD_BRANCH" \
   --body "PR initialized by patchdog" \
-  --head "${PATCHDOG_BRANCH}" \
-  --base "${BASE_BRANCH}" \
-  --assignee "${ASSIGNEE}" \
+  --head "$PATCHDOG_BRANCH" \
+  --base "$BASE_BRANCH" \
+  --assignee "$ASSIGNEE" \
   --draft
