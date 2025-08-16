962192eaf2fad48c499469664b61cbe1d7762e84
Yurii Litvinov <litvinov.yura@gmail.com>
2025-08-16 23:25:54 +0200

    updated runner

--- run.sh
+++ run.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e
 
-echo "NAME: $COMMIT_NAME $COMMIT_EMAIL "
+#Configure user
+git config user.email "$COMMIT_EMAIL" 
+git config user.name "$COMMIT_NAME"
 
 git fetch origin ${BASE_BRANCH}
 
 #Test if PR contains conflict and abort any further actions
-if ! git merge --no-commit --no-ff origin/${BASE_BRANCH}; then
-    exit 1
-fi
+git merge --no-commit --no-ff origin/"$BASE_BRANCH" || exit 1
+git merge --abort
 
 #Download and run latest release
 curl -L -o patchdog-linux-x86_64 https://github.com/YuraLitvinov/patchdog/releases/latest/download/patchdog
@@ -18,15 +19,11 @@ git diff origin/$BASE_BRANCH...$HEAD_BRANCH > base_head.patch
 #Cleanup artifacts
 rm base_head.patch && rm patchdog-linux-x86_64
 
-#Configure user
-git config --global user.email "$COMMIT_EMAIL" 
-git config --global user.name "$COMMIT_NAME"
-
 #Create a unique pull request
 PATCHDOG_BRANCH="patchdog-$(uuidgen)"
-
 git checkout -b "$PATCHDOG_BRANCH"
 git add . ':(exclude).github/workflows/'
+
 git commit -m "Patchdog-included changes for $HEAD_BRANCH"
 git push origin $PATCHDOG_BRANCH
 gh pr create \
