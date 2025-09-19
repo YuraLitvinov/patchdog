2a32c504c7889a25a31df1d6098eb8bce1c9c71d
YuraLitvinov <litvinov.yura@gmail.com>
2025-09-19 16:31:46 +0200

    Fail (#67)
    * updated run.sh
    
    * polished
    
    * git diff UB

--- run.sh
+++ run.sh
@@ -13,7 +13,6 @@ ASSIGNEE=$(jq -r '.author.login'  <<< "$PR_JSON")
 git config user.email "$COMMIT_EMAIL" 
 git config user.name "$COMMIT_NAME"
 
-git fetch origin "$HEAD_BRANCH":"$HEAD_BRANCH"
 git switch "$HEAD_BRANCH"
 
 #Test if PR contains conflict and abort any further actions
@@ -22,12 +21,13 @@ if [ -f .git/MERGE_HEAD ]; then
   git merge --abort
 fi
 
-
-
 #Download and run latest release
 curl -L -o patchdog-linux-x86_64 https://github.com/YuraLitvinov/patchdog/releases/latest/download/patchdog-linux-x86_64
 chmod +x patchdog-linux-x86_64
-git diff origin/$BASE_BRANCH...origin/$HEAD_BRANCH > base_head.patch
+git diff $BASE_BRANCH...$HEAD_BRANCH > base_head.patch
+echo $BASE_BRANCH
+echo $HEAD_BRANCH
+echo $ASSIGNEE
 ./patchdog-linux-x86_64 --file-patch base_head.patch
 #Cleanup artifacts
 rm base_head.patch && rm patchdog-linux-x86_64
@@ -36,18 +36,16 @@ rm base_head.patch && rm patchdog-linux-x86_64
 PATCHDOG_BRANCH="patchdog-$(date +%s)"
 git switch -c "$PATCHDOG_BRANCH"
 git add . 
+git commit -m "Patchdog-included changes for $HEAD_BRANCH"
+git push origin "$PATCHDOG_BRANCH"
 
-if ! git diff --cached --quiet; then
-    git commit -m "Patchdog-included changes for $HEAD_BRANCH"
-    git push origin "$PATCHDOG_BRANCH"
+# Authenticate GH CLI and create PR
+echo "$GH_TOKEN" | gh auth login --with-token
+gh pr create \
+    --title "Patchdog merge into $HEAD_BRANCH" \
+    --body "PR initialized by patchdog" \
+    --head "$PATCHDOG_BRANCH" \
+    --base "$HEAD_BRANCH" \
+    --assignee "$ASSIGNEE" \
+    --draft
 
-    # Authenticate GH CLI and create PR
-    echo "$GH_TOKEN" | gh auth login --with-token
-    gh pr create \
-        --title "Patchdog merge into $HEAD_BRANCH" \
-        --body "PR initialized by patchdog" \
-        --head "$PATCHDOG_BRANCH" \
-        --base "$HEAD_BRANCH" \
-        --assignee "$ASSIGNEE" \
-        --draft
-fi
